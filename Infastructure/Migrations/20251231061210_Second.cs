e032be1d1856a1d2a0035ffab5762e58d39b6269
GitNub-Community-1 <maxik010020003@gmail.com>
2025-12-31 11:14:54 +0500

    Changed name of field

---
+++ Infastructure/Migrations/20251231061210_Second.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserNotifications_NotificationTypes_NotificationTypeId",
+                table: "UserNotifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserNotifications_NotificationTypeId",
+                table: "UserNotifications");
+
+            migrationBuilder.DropColumn(
+                name: "NotificationTypeId",
+                table: "UserNotifications");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserNotifications_NotificationId",
+                table: "UserNotifications",
+                column: "NotificationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserNotifications_NotificationTypes_NotificationId",
+                table: "UserNotifications",
+                column: "NotificationId",
+                principalTable: "NotificationTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserNotifications_NotificationTypes_NotificationId",
+                table: "UserNotifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserNotifications_NotificationId",
+                table: "UserNotifications");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NotificationTypeId",
+                table: "UserNotifications",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserNotifications_NotificationTypeId",
+                table: "UserNotifications",
+                column: "NotificationTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserNotifications_NotificationTypes_NotificationTypeId",
+                table: "UserNotifications",
+                column: "NotificationTypeId",
+                principalTable: "NotificationTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
